a88aee0634a8d95f4aa40f4e0bf00ff6f403e25d
Francis Gonzalez <alexandergonzalez92@hotmail.com>
2019-10-19 20:23:23 +0200

    cambio version entity

--- ProyectoFinal_ProgramacionCShearp/Model1.Designer.cs
+++ ProyectoFinal_ProgramacionCShearp/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\KMC\Google Drive\Proyectos Visual Studio 2019\ProyectoFinal_ProgramacionCShearp\ProyectoFinal_ProgramacionCShearp\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Fcc tarde\Source\Repos\kitomc\ProyectoFinal_ProgramacionCShearp\ProyectoFinal_ProgramacionCShearp\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
